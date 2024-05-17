da38cefd2e518947dd0fb4d753d98bde78edf1d7
panxhot01@gmail.com <Panxhot01>
2024-05-17 06:04:03 -0400

    Se añade menu y footer por JS

---
+++ assets/js/navbar.js
@@ -0,0 +1,14 @@
+$( document ).ready(function(){
+    let navbar = `<nav class="navbar navbar-expand-lg navbar">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link"><img src="./assets/img/icon/El_Carvajal.ico" width="35px" alt="ICONO"></a></li>
+        <li class="nav-item"><a class="nav-link" href="index.html">INICIO</a></li>
+        <li class="nav-item"><a class="nav-link" href="productos.html">PRODUCTOS</a></li>
+        <li class="nav-item"><a class="nav-link" href="suegrencias.html">SUGERENCIAS</a></li>
+        <li class="nav-item"><a class="nav-link" href="contacto.html">CONTACTO</a></li>
+      </a>
+    </ul>
+  </nav>`;
+    $("#menu-principal").html(navbar);
+});
